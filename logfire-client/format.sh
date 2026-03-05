4eae40174b1892dfd9fa468927a59a3924e8e3e2
Marc Brinkmann <marc@pydantic.dev>
2026-03-05 15:34:04 +0100

    Remove nix-related files from logfire-client

--- logfire-client/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#: Formats the source.
-#: Uses --config to override rustfmt settings without nightly toolchain.
-#: As a little hack, supports `--check`.
-
-cargo fmt -- --config group_imports=StdExternalCrate --config imports_granularity=Crate $@
-nixfmt $@ flake.nix
